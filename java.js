d058d8be734359ebb3db097ac2658f4d0721fff7
gabrys-ei19 <102586104+gabrys-ei19@users.noreply.github.com>
2022-09-13 07:52:22 +0200

    update

--- java.js
+++ java.js
@@ -1,16 +1,16 @@
-imie = "sebastian";
-nazwisko = Array('G','A','B','R','Y','S');
-odwrot = "";
-wynik = "";
-function ImieNazw (tekst = "") {
-  i = 0;
-  wynik = "";
-  while(tekst[i] != undefined) {
+let imie = 'sebastian';
+let nazwisko = Array('G', 'A', 'B', 'R', 'Y', 'S');
+imie = imie.reverse();
+nazwisko = nazwisko.reverse();
+let wynik = '';
+function ImieNazw(tekst = '') {
+  let i = 0;
+  wynik = '';
+  while (tekst[i] != undefined) {
     wynik += tekst[i];
-    i++
+    i++;
   }
   return wynik;
 }
-wynik = ImieNazw(imie) +" "+ImieNazw(nazwisko)
-odwrot = revers(wynik)
-console.log(odwrot);
+wynik = ImieNazw(imie) + ' ' + ImieNazw(nazwisko);
+console.log(wynik);
