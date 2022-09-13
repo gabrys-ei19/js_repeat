126cbe25123289efe79bcde39ba499d2cdfc7090
gabrys-ei19 <102586104+gabrys-ei19@users.noreply.github.com>
2022-09-13 08:59:04 +0200

    rekurencja

--- fibonaci.js
+++ fibonaci.js
@@ -1,11 +1,14 @@
-x = 0;
-y = 1;
-ile = parseInt(prompt("Podaj ilość liczb w ciągu: ", "9"))
-console.log('Ciąg Fibonacciego:');
+const ile = parseInt(prompt("Podaj ilość liczb w ciągu: ", "9"))
+let x;
+function fibonaci(x){
+  if(x<2){
+    return x;
+  }else{
+    return fibonaci(x-1)+fibonaci(x-2);
+  }
+}
 
-for (i = 1; i <= ile; i++) {
-    console.log(x);
-    z = x + y;
-    x = y;
-    y = z;
+console.log('Ciąg Fibonacciego:');
+for (i = 0; i < ile; i++) {
+    console.log(fibonaci(i));
 }
