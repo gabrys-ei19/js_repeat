dd690e30995d4b1713ec7f35cd606429d3ea2371
gabrys-ei19 <102586104+gabrys-ei19@users.noreply.github.com>
2022-09-13 08:13:44 +0200

    prompt

--- fibonaci.js
+++ fibonaci.js
@@ -1,8 +1,9 @@
 x = 0;
 y = 1;
+ile = parseInt(prompt("Podaj ilość liczb w ciągu: ", "9"))
 console.log('Ciąg Fibonacciego:');
 
-for (let i = 1; i <= 8; i++) {
+for (let i = 1; i <= ile; i++) {
     console.log(x);
     z = x + y;
     x = y;
